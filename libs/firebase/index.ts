d9ea1d2b507dd3c57571d38c8ddf129f90568a5a
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2022-10-10 08:33:24 -0400

    firebase init

---
+++ libs/firebase/index.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDc4zB6BOS67tJHG05bFfI1I1UcppfktHs',
+  authDomain: 'chores-e975a.firebaseapp.com',
+  projectId: 'chores-e975a',
+  storageBucket: 'chores-e975a.appspot.com',
+  messagingSenderId: '562800463987',
+  appId: '1:562800463987:web:1457904dc892e981c89a06',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
