823ad11b0d35ac53be8ee04c77a122024af4e5b7
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2022-11-30 17:07:58 -0500

    attempt build fix

--- components/base/ListItem/index.tsx
+++ components/base/ListItem/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import Button from '../Button';
-import styles from './ListItem.module.css';
+// import styles from './ListItem.module.css';
 
 export interface ListItemProps {
   children: React.ReactNode;
@@ -11,10 +11,51 @@ const ListItem: FC<ListItemProps> = ({ children, onClick }) => {
   if (onClick)
     return (
       <Button type="invisible" onClick={onClick}>
-        <div className={styles.listItem}>{children}</div>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            padding: 10,
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            paddingLeft: 10,
+            paddingRight: 10,
+            columnGap: 10,
+          }}
+        >
+          {children}
+        </div>
       </Button>
     );
-  return <div className={styles.listItem}>{children}</div>;
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'row',
+        padding: 10,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        paddingLeft: 10,
+        paddingRight: 10,
+        columnGap: 10,
+      }}
+    >
+      {children}
+    </div>
+  );
 };
 
 export default ListItem;
+
+const styles = {
+  listItem: {
+    display: 'flex',
+    flexDirection: 'row',
+    padding: 10,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingLeft: 10,
+    paddingRight: 10,
+    columnGap: 10,
+  },
+};
