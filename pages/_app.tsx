92fc84ce012136365bd6254035a0125be90f569e
Devin Cenatiempo <dcenatiempo@insiderealestate.com>
2022-12-02 22:24:00 -0500

    persistGate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,20 +6,26 @@ import Firebase from '../components/util/Firebase';
 import ThemeListener from '../components/util/ThemeListener';
 import ScreenSizeListener from '../components/util/ScreenSizeListener';
 import Head from 'next/head';
+import { PersistGate } from 'redux-persist/integration/react';
+import { persistStore } from 'redux-persist';
+
+let persistor = persistStore(store);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
-      <Head>
-        <meta
-          name="viewport"
-          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
-        />
-      </Head>
-      <ThemeListener />
-      <Firebase />
-      <ScreenSizeListener />
-      <Component {...pageProps} />
+      <PersistGate loading={null} persistor={persistor}>
+        <Head>
+          <meta
+            name="viewport"
+            content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+          />
+        </Head>
+        <ThemeListener />
+        <Firebase />
+        <ScreenSizeListener />
+        <Component {...pageProps} />
+      </PersistGate>
     </Provider>
   );
 }
